30463a236fef3ff528a20347b1359a20e34e65dc
Joe Stephens <jstephens9245@gmail.com>
2018-12-21 21:10:11 -0500

    modified webpack corrections, still debuging solution

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -65,9 +65,6 @@ var config = {
     path      : paths.dist,
     publicPath: '/'
   },
-  optimization: {
-    minimizer: [new TerserPlugin()],
-  },
   plugins: [
     extractSass,
     new HtmlWebpackPlugin({
